607b81b25a2f478ef6b929a45db3ca415924e85a
Shashwat Pandey <105585782+Shashwat7012@users.noreply.github.com>
2024-06-16 16:23:06 +0530

    Update emailService.js

--- nodemailer/emailService.js
+++ nodemailer/emailService.js
@@ -14,8 +14,8 @@ const sendRegistrationEmail = async (to, userName,busName,busNumber,seatNumber,f
   const mailOptions = {
     from: process.env.EMAIL,
     to,
-    subject: 'Welcome to BookmyBus',
-    text: `Hello ${userName},\n\nThank you for registering at BookmyBus. We are excited to have you on board.\n\nBest regards,\nThe BookmyBus Team\n Your Bus name is  ${busName}.\n Bus Number :- ${busNumber}\n Your selected seat number is ${seatNumber},\nBus starts from :- ${from},\n Your destination :- ${destination},\n JourneyDate :- ${journeyDate},\n arrival Time :- ${arrivalTime},\n departure Time :- ${departureTime}`
+    subject: 'Welcome to swift-track',
+    text: `Hello ${userName},\n\nThank you for registering at swift-track. We are excited to have you on board.\n\nBest regards,\nThe BookmyBus Team\n Your Bus name is  ${busName}.\n Bus Number :- ${busNumber}\n Your selected seat number is ${seatNumber},\nBus starts from :- ${from},\n Your destination :- ${destination},\n JourneyDate :- ${journeyDate},\n arrival Time :- ${arrivalTime},\n departure Time :- ${departureTime}`
   };
 
   try {
